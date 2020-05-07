6135cc44e0c1ad876fd75e9ae3e790fb5cb4e3e6
thecurrentuser <thecurrentuser@live.com>
2020-05-07 09:12:26 -0300

    rem old files

--- src/controller/IndexController.ts
+++
@@ -1,8 +0,0 @@
-//Tem que tipar as varíaveis do express
-import { Request, Response } from 'express'
-
-export default {
-    async index(req: Request, res: Response) {
-        return res.json("Maratona Full Cycle 2.0")
-    }
-}
